7f0b8fefe6969a995d1701fe01237d7d6e8db387
Caviac <Caviac.Mail@gmail.com>
2016-11-28 19:25:30 -0500

    Project

---
+++ Project/src/lists.js
@@ -0,0 +1,25 @@
+var traits = [
+	{
+		title: "Aggressive",
+		text: "This character takes all criticism personally.",
+	},
+	{
+		title: "Altruistic",
+		text: "This character loves to help others. They regain 2 Vitality every time they help another person.",
+	},
+	{
+		title: "Barbaric",
+		text: "This character deals 1 Bonus Damage and has 1 extra Movement Speed while equipping only old-age weapons and armor.",
+	}
+];
+
+var items = [
+	{
+		title: "Bio-Engineered Beans",
+		text: "($25) When watered, grows into a massive beanstalk.",
+	},
+	{
+		title: "Malfunctioning Teleporter",
+		text: "($1000, Intelligent) When used, roll 4 times. The first 2 rolls decide vertical and horizontal direction respectively - evens mean up/left, odds mean down/right. The next 2 rolls are the number of tiles to move in those directions in the order rolled. If the user would land on an uninhabitable space, move to the nearest habitable one. Breaks after use.",
+	}
+];
